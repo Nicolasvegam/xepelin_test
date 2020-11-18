bf66c29c26bcb8661ef2d62a6ed530d88618f08c
Nicolasvegam <profemiguel@MacBook-Air-de-Profe.local>
2020-11-18 03:20:24 -0300

    hosts

--- xepelin/settings.py
+++ xepelin/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '2p69*@1-7&hkgsh158h3_2+%0ea(db
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['https://xepelin-test.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['xepelin-test.herokuapp.com']
 
 
 # Application definition
